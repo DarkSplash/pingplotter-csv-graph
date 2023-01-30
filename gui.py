b77117a9046c68561075f2e60f89a813f8407746
DarkSplash <amswinford@gmail.com>
2023-01-30 15:51:45 -0600

    menu with GUI reinitialization added

--- gui.py
+++ gui.py
@@ -1,19 +1,32 @@
 from tkinter import *
 from tkinter.ttk import *
 
+def labelRename(lbl:Label, newString:str):
+    lbl.configure(text=newString)
+
+
+def restart(root):
+    root.destroy()
+    main()
+
+
 def main():
     root = Tk()
-    root.geometry("300x300")
     root.title("PingPlotter CSV Grapher")
-    
-    frame = Frame(root)
-    frame.grid()    
-    
-    label = Label(root, text ="Graph Selector").grid()
+    root.geometry("300x300")
 
-    button = Button(frame, text ='Graph All Hops') 
-    button.grid()     
-    
+    menu = Menu(root)
+    item = Menu(menu)
+    item.add_command(label='Back to Main Menu', command=lambda: restart(root))
+    menu.add_cascade(label='Menu', menu=item)
+    root.config(menu=menu)
+
+    label = Label(root, text ="Graph Selector")
+    label.grid()
+
+    button = Button(root, text ='Graph All Hops',command=lambda: labelRename(lbl=label, newString="a")) # Need lambda to call function w/ parameter and not have it execute on runtime
+    button.grid()
+
     root.mainloop()
 
 
