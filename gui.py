05698c481b0009f8a5d7b35434ced5d9050a5939
DarkSplash <amswinford@gmail.com>
2023-01-30 16:09:26 -0600

    modularized the functions

--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+import graph
+
 from tkinter import *
 from tkinter.ttk import *
 
@@ -5,29 +7,43 @@ def labelRename(lbl:Label, newString:str):
     lbl.configure(text=newString)
 
 
-def restart(root):
-    root.destroy()
-    main()
-
-
-def main():
-    root = Tk()
-    root.title("PingPlotter CSV Grapher")
-    root.geometry("300x300")
-
+def addMenu(root):
     menu = Menu(root)
     item = Menu(menu)
     item.add_command(label='Back to Main Menu', command=lambda: restart(root))
     menu.add_cascade(label='Menu', menu=item)
     root.config(menu=menu)
 
+
+def restart(root):
+    """
+    Function to kill the current tkinter root window and recall the main method
+    """
+    root.destroy()
+    mainMenu()
+
+
+
+def mainMenu():
+    root = Tk()
+    root.title("PingPlotter CSV Grapher")
+    root.geometry("300x300")
+
+    addMenu(root)
+
     label = Label(root, text ="Graph Selector")
     label.grid()
 
-    button = Button(root, text ='Graph All Hops',command=lambda: labelRename(lbl=label, newString="a")) # Need lambda to call function w/ parameter and not have it execute on runtime
+    button = Button(root, text ='Graph All Hops',command=lambda: graph.graphAll(graph.getDataFrame())) # Need lambda to call function w/ parameter and not have it execute on runtime
     button.grid()
 
     root.mainloop()
+
+
+
+def main():
+    mainMenu()
+
 
 
 if __name__ == "__main__":
